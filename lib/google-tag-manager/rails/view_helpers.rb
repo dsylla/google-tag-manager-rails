586ded16ddb54128572a9a2d1f500f27672c2a2d
David Sylla <d.sylla@xair.fr>
2016-06-14 14:35:27 +0200

    upgrade google tag manager

--- lib/google-tag-manager/rails/view_helpers.rb
+++ lib/google-tag-manager/rails/view_helpers.rb
@@ -3,7 +3,7 @@ require 'active_support/core_ext/string/output_safety'
 module GoogleTagManager::Rails
   module ViewHelpers
     def google_tag_manager
-      return "" unless GoogleTagManager.valid_gtm_id?
+      return "" unless GoogleTagManager.valid_gtm_info?
 
       GoogleTagManager.to_html.html_safe
     end
